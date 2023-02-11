23e9f8ace5907c972138234a604b7c24500664a5
Trenton Ornelas <trentonornelas@gmail.com>
2023-02-11 17:16:36 -0600

    return basic HTML5 hello world

--- single_threaded_server/src/main.rs
+++ single_threaded_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -17,12 +18,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
-        .map(|result| result.unwrap()) 
+        .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    // implement sending data in response to a client request.
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
